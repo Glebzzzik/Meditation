955072aac8b79229c04535b7c4ceb09b131195ec
gleb <glebgi23@mail.ru>
2022-03-14 22:13:38 +0300

    Initial Commit

---
+++ Meditation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meditation
+//
+//  Created by Глеб Голощапов on 14.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
